6446bd9d9a04fa7a1fc24efc24371a7bf9ffc1c8
Parzival <emanuelpedro267@gmail.com>
2025-12-18 20:45:54 -0300

    primeirasEscolhas
    Criação dos botões Comercial e Residencial

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+
+//Aba Orçamento
+
 let iconOrcamento = document.querySelector('.iconCart');
+let fecharOrcamento = document.querySelector('.fechar')
 let body = document.querySelector('body');
 
 
 iconOrcamento.addEventListener('click', () => {
     body.classList.toggle('mostrarOrcamento')
 })
+
+fecharOrcamento.addEventListener('click', () => {
+    body.classList.toggle('mostrarOrcamento')
+})
+
+
+//Residencial ou Comercial 
+
+const opcoes = document.querySelectorAll('.opcao');
+
+opcoes.forEach(opcao => {
+    opcao.addEventListener('click', () => {
+
+        // remove seleção de todas
+        opcoes.forEach(o => o.classList.remove('selecionado'));
+
+        // adiciona apenas na clicada
+        opcao.classList.add('selecionado');
+
+        // pega o valor
+        const valor = opcao.dataset.value;
+        console.log('Selecionado:', valor);
+    });
+});
+
+
+
+ 
