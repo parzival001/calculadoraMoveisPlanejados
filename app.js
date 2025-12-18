bf5e39cc01f6ab43d1cf63fe88196f204f9d3f75
Parzival <emanuelpedro267@gmail.com>
2025-12-18 19:57:36 -0300

    upload 01

---
+++ app.js
@@ -0,0 +1,7 @@
+let iconOrcamento = document.querySelector('.iconCart');
+let body = document.querySelector('body');
+
+
+iconOrcamento.addEventListener('click', () => {
+    body.classList.toggle('mostrarOrcamento')
+})
